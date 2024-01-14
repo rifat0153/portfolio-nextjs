32b3bf4506c81de761667329fed67c7e90d9a0f0
Mahbubur Rahman <rifat0153@gmail.com>
2024-01-14 17:00:16 +0000

    Update dependencies and remove experimental appDir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  experimental: {},
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
