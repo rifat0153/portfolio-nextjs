a7490511b67804e13e97aba0a57c0928a722a678
Mahbubur Rahman <rifat0153@gmail.com>
2024-02-04 16:51:21 +0000

    Add SkillsCarousel button and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,27 @@ import { Suspense } from 'react';
 export default function Home() {
   return (
     <>
-      <div className='grid grid-cols-12 gap-4 gap-y-20 pt-8 lg:gap-20 lg:gap-y-0 lg:pt-[30vh]'>
-        <div className='col-span-12 break-words text-left text-medium leading-[1rem] sm:text-lg lg:col-span-8'>
-          <p>
+      <div className='grid grid-cols-12 gap-4 gap-y-20 pb-20 pt-8 sm:pb-[35vh] lg:gap-20 lg:gap-y-0 lg:pt-[30vh]'>
+        <div className='col-span-12 sm:text-xl lg:col-span-8'>
+          <p className='break-words text-left leading-7 sm:leading-10'>
             Hi, I am
-            <span className='text-2xl font-bold text-cyan-500 sm:text-5xl'> Mahbubur</span>, a
-            full-stack web/mobile developer. Proficient in
+            <span className='text-2xl font-bold leading-10 text-fuchsia-600 sm:text-5xl'>
+              &nbsp;Mahbubur
+            </span>
+            , a full-stack web/mobile developer.
+            {/* <span className='text-lg font-bold text-rose-900 sm:text-xl'> Specializing</span> in */}
+            &nbsp;Specializing in
             <span className='text-lg font-bold text-blue-600 sm:text-xl'> .Net </span>,
             <span className='text-lg font-bold text-lime-600 sm:text-xl'> Vue.js </span>,
-            <span className='text-lg font-bold text-blue-800 sm:text-xl'> React.js </span>, and
-            <span className='text-lg font-bold text-purple-600 sm:text-xl'> Flutter </span>. Beyond
+            <span className='text-lg font-bold text-cyan-600 sm:text-xl'> React.js </span>, and
+            <span className='text-lg font-bold text-blue-600 sm:text-xl'> Flutter </span>. Beyond
             standard development, I&apos;ve dabbled in Python-based machine learning, which has led
             to several key publications.
           </p>
+
+          <div className='pt-4 lg:pt-4'>
+            <SocialLinks />
+          </div>
         </div>
 
         <div className='col-span-12 inline-flex items-center justify-center text-center lg:col-span-4'>
@@ -29,11 +37,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='px-4 py-32 lg:px-0 lg:pb-[35vh] lg:pt-8'>
+      {/* <div className='px-4 py-32 lg:px-0 lg:pb-[35vh] lg:pt-8'>
         <SocialLinks />
-      </div>
+      </div> */}
 
-      <section className=''>
+      <section>
         <Disclaimer />
       </section>
     </>
