6c32f1eb370700cf6080f5d6a7114d0d385bd6f9
Mahbubur Rahman <rifat0153@gmail.com>
2024-02-10 23:09:38 +0000

    Update meta description in head.tsx and import Head component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { AnalyticsWrapper } from '@/components/analytics';
-import Header from '@/components/Header';
-import { Alegreya, Inter } from 'next/font/google';
-import './globals.css';
-
 import { NextUIProvider } from '@nextui-org/react';
-import DynamicBreadcrumbs from '@/components/DynamicBreadcrumbs';
 import { useRouter } from 'next/navigation';
+
+import Header from '@/components/Header';
+import { AnalyticsWrapper } from '@/components/analytics';
+import { Alegreya, Inter } from 'next/font/google';
+import DynamicBreadcrumbs from '@/components/DynamicBreadcrumbs';
+import Head from './head';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const alegreya = Alegreya({ subsets: ['latin'], variable: '--font-alegreya' });
@@ -21,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <Head />
 
       <body
         className={`
