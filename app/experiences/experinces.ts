a70f9bd0a6dafa2ed5b4fd86c0d9fd17165724b3
Mahbubur Rahman <rifat0153@gmail.com>
2024-08-04 17:32:46 +0100

    Refactor project IDs in projects.ts and update project descriptions

--- app/experiences/experinces.ts
+++ app/experiences/experinces.ts
@@ -3,7 +3,7 @@ import { getSkillsByExperienceId, skillIDs, SkillIDType } from '../skills/skills
 
 export const experienceIDs = {
   Senceive: 'senceive',
-  FlutterFreelance: 'flutter-freelance',
+  FlutterFreelance: 'flutter',
   PlanBInsights: 'plan-b-insights',
   BengalSoftware: 'bengal-software',
 } as const;
@@ -49,29 +49,14 @@ export const workExperiences: WorkExperience[] = [
     duration: 'June 2022 – Current',
     country: 'United Kingdom',
     description:
-      'Full-stack developer working on a range of projects including the web monitoring platform, signal box, and production dashboard utilizing .Net, Vue, Nuxt, and TypeScript.',
+      'Full-stack C# developer working on a range of projects utilizing .Net, Vue, Nuxt, and TypeScript.',
     bulletPoints: [
-      `Server development with .Net exposing REST API for frontend clients. Followed best practices
-      including clean architecture, mediator pattern, SOLID principles etc that led to maintainable and
-      testable code.`,
-
-      `SPA/App development with Vue.js and Nuxt.js. Used pinia and vue composables for state
-      management, TailwindCss/CSS for styling, Typescript plus Zod for data validation and strict
-      typing and Tanstack Query for advanced asynchronous operations and caching.`,
-
-      `Took initiative in designing and refactoring major products, optimizing architectures for
-      substantial gains in performance and reliability.`,
-      `Led the redesign and API overhaul of the flagship data/alert management/monitoring tool,
-      transitioning from .NET 5 to .NET 8.`,
-
-      `Designed and developed an full-stack anomaly detection system for Network Rail, contributing to
-      remote monitoring and early problem detection, thereby improving the reliability and performance
-      of rail systems.`,
-
-      `Utilized Python and Open AI to generate C# Dapper boilerplate code for interacting with stored
-      procedures in the database, streamlining development processes and enhancing code efficiency.`,
-      `Created internal dashboards for statistical data analysis, providing a unified source for product
-      performance assessment and actionable insights.`,
+      `Architected and developed high-performance REST APIs and backend systems using .NET, C#, Dapper, and EF, deployed on Azure with Docker containerization.`,
+      `Spearheaded the development of cutting-edge SPAs using Vue.js, SCSS, Tailwind, and TypeScript, significantly enhancing product functionality and web accessibility.`,
+      `Led the development of a mission-critical anomaly detection system for Network Rail, substantially improving monitoring and reliability of the UK rail system.`,
+      `Orchestrated a major backend migration to .NET 8, implementing SOLID principles, Functional Design Patterns, DDD, and clean architecture, resulting in a 40% performance boost for data-intensive operations.`,
+      `Developed advanced image detection and segmentation models using YOLO, TensorFlow, and Python to enhance object detection capabilities on train tracks.`,
+      `Designed and implemented internal dashboards for statistical analysis, driving early issue identification and performance enhancements in production.`,
     ],
     image:
       'https://firebasestorage.googleapis.com/v0/b/portfolio-6b79f.appspot.com/o/senceive-1.png?alt=media&token=aec7c648-318c-4757-8275-6266b7cfeff7',
@@ -80,23 +65,17 @@ export const workExperiences: WorkExperience[] = [
   },
   {
     id: experienceIDs.FlutterFreelance,
-    jobTitle: 'Flutter Lead & Developer (Contract / Remote)',
-    company: 'Contract / Remote',
+    jobTitle: 'Flutter Developer',
+    company: 'RSI Lab',
     duration: 'January 2022 – May 2023',
-    country: 'Sweden, Bangladesh, United Kingdom',
-    description: 'Developed and maintained mobile applications for various clients.',
+    country: 'Dhaka, Bangladesh',
+    description: 'Developed and maintain IOS and Android mobile applications built with Flutter.',
     bulletPoints: [
-      `Developed 4 mobile applications over two years using both Flutter and React Native`,
-
-      `Led the development of a client-specific restaurant application at Elvan AB, mentoring a team of
-      two Flutter developers, and transitioning to a mono repo structure, resulting in streamlined project
-      management and enhanced efficiency`,
-
-      `Developed the latest "The Daily Star - Bangladesh" app at RSI LAB, leading to 100k+ downloads,
-      10,000+ daily active users, and a 70% reduction in Android device crashes`,
-
-      `Collaborated with the development team at Spotlas to address and rectify software bugs,
-      ensuring app reliability and solidifying user trust.`,
+      `Led the development of "The Daily Star - Bangladesh'' app using Flutter and Riverpod, achieving 100k+ downloads, 10,000+ daily active users, and a 70% reduction in Android device crashes.`,
+      `Successfully implemented the pre-existing Figma design system into the app, ensuring a consistent and visually appealing user interface across all features.`,
+      `Optimized app performance by implementing custom SliverLists, employing efficient caching strategies, and offloading work from the main thread, resulting in significant reduction of UI jank and improved overall user experience.`,
+      `Managed feature flags for different categories of news, allowing for flexible and customizable content delivery.`,
+      `Continuously identified and resolved bugs, maintaining high app stability and user satisfaction.`,
     ],
     associatedSkills: ['flutter', 'dart', 'nodejs', 'rest-api', 'firebase', 'mongodb'],
     associatedProjects: ['elvan-restaurant-app', 'daily-star-app', 'spotlas'],
@@ -108,14 +87,13 @@ export const workExperiences: WorkExperience[] = [
     country: 'Bangladesh',
     duration: 'January 2021 – September 2021',
     description:
-      'Junior software developer tasked with developing and maintaining the Plan B Insights mobile application and backend services.',
+      'Full Stack Developer responsible for creating mobile applications and backend services, ensuring seamless integration and high performance.',
     bulletPoints: [
-      `Developed a charity app with Flutter with integrated geospatial features, enhancing user
-       engagement and enabling real-time location-based notifications.`,
-
-      `Developed REST APIs with express.js and mongodb.`,
-
-      `Utilized docker compose for easy deployment on Google Cloud Run.`,
+      `Developed Aalokito, a location-based charity application for food and clothing distribution, utilizing Flutter, GetX, Firebase, and REST APIs for the frontend.`,
+      `Implemented user authentication and authorization using Firebase Auth, ensuring secure access to the application.`,
+      `Created robust backend systems using Node.js, Express, and MongoDB, implementing RESTful APIs to support the mobile application.`,
+      `Implemented Docker containerization with Docker Compose, streamlining environment setup and enabling efficient deployment to Google Cloud Run.`,
+      `Gained hands-on experience in full-stack development, from mobile app creation to backend services, authentication, and cloud deployment.`,
     ],
     image:
       'https://firebasestorage.googleapis.com/v0/b/portfolio-6b79f.appspot.com/o/plan%20b%20insights%201.png?alt=media&token=e7faf080-208c-44c7-8303-0578a742b816',
@@ -138,10 +116,10 @@ export const workExperiences: WorkExperience[] = [
     duration: 'September 2020 – December 2020',
     country: 'Bangladesh',
     description:
-      'Worked as a software intern, working on React and Node.js projects, and learning about the software development process.',
+      'Software Intern focused on React and Node.js projects, gaining hands-on experience in the software development process.',
     bulletPoints: [
-      `Created web pages with React/Redux with RESPI API integration via Axios.`,
-      `Learned the basics of Node.js and REST API development`,
+      `Contributed to ongoing projects by developing and maintaining web components using React.js and CSS.`,
+      `Rapidly gained proficiency in React.js, Node.js, and REST API development, applying industry best practices.`,
     ],
     image:
       'https://firebasestorage.googleapis.com/v0/b/portfolio-6b79f.appspot.com/o/bengal-soft.png?alt=media&token=b0b0b1df-2db5-4c21-9f8a-c200a05764fb',
